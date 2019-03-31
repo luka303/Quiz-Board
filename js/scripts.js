de6d1c9584106bd2bb4b11ab77239e19cd1466fc
luka wandanje <lukawandanje@gmail.com>
2019-03-31 10:18:23 +0300

    add to the javascript and css files

---
+++ js/scripts.js
@@ -0,0 +1,18 @@
+$(function() {
+  $("#start-btn").click(function(event) {
+    event.preventDefault();
+    var userFirstName = $("#firstName").val();
+    var userSecondName = $("#secondName").val();
+     if(userFirstName.length === 0 || userSecondName.length === 0) {
+      $("#no-name").show();
+      $("#firstName").addClass("enter-name");
+      $("#secondName").addClass("enter-name");
+    } else {
+      $("#intro").hide();
+      $("#quizpage").show();
+    }
+		$(document).ready(function() {
+		  $("p").click(function() {
+		    $("#quizpage").show();
+		  });
+		});
